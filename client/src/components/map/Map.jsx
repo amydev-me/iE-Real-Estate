950522dfb409b2cbbd58067aa2446e59105c0cf5
amypyae <ampyaephyonaing@gmail.com>
2024-06-13 16:21:39 +0800

    Bind Lat&Lng

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -9,7 +9,7 @@ function Map({ items }) {
     });
 
     return (
-        <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={false} className="map">
+        <MapContainer center={items.length === 1 ? [items[0].latitude, items[0].longitude]:[51.505, -0.09]} zoom={7} scrollWheelZoom={false} className="map">
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
