aac1e4efce052b0e5f27a7aa78425dce48a0d7d3
amypyae <ampyaephyonaing@gmail.com>
2024-06-14 18:06:32 +0800

    Declare chat & message route

--- api/app.js
+++ api/app.js
@@ -6,6 +6,8 @@ import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messagesRoute from "./routes/message.route.js";
 
 const port = process.env.PORT || 3000;
 
@@ -20,6 +22,8 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/test', testRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/chats', chatRoute);
+app.use('/api/messages', messagesRoute);
 
 app.listen(port, () => {
     console.log('Server is running, 8800');
