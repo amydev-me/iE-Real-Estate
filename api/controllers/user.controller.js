8bed514f0014fe3bcf2f65188a6f78adc023c848
amypyae <ampyaephyonaing@gmail.com>
2024-06-14 18:04:05 +0800

    Saving User's Post

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -100,4 +100,79 @@ const deleteUser = async(req, res) => {
     }
 }
 
-export { getUsers, getUser, updateUser, deleteUser };
+
+const savePost = async (req, res) => {
+    const postId = req.body.postId;
+    const tokenUserId = req.userId; 
+ 
+    try{
+
+        const savedPost = await prisma.savedPost.findUnique({
+            where: {
+              userId_postId: {
+                userId: tokenUserId,
+                postId,
+              },
+            },
+          });
+
+        if(savedPost) {
+            await prisma.savedPost.delete({
+                where: {
+                    id: savedPost.id
+                }
+            })
+
+            res.status(200).json({ message: "Post removed from saved list"})
+        }else{
+            await prisma.savedPost.create({
+                                 
+                data: { 
+                    userId: tokenUserId,
+                    postId
+                }
+            });
+
+
+            res.status(200).json({ message: "Post saved."})
+        }
+
+       
+    }catch(error) {
+        console.log(error);
+        res.status(500).send({ message: "Failed to save post." });
+    }
+}
+
+const profilePosts = async(req, res) => {
+    const tokenUserId = req.userId; 
+
+    try{
+        const userPosts = await prisma.post.findMany({
+            where: { 
+                userId: tokenUserId
+             } 
+        });
+
+        const saved = await prisma.savedPost.findMany({
+            where: { 
+                userId: tokenUserId
+            },
+            include : {
+                post: true
+            }
+        });
+
+        const  savedPosts = saved.map(item => item.post);
+
+        res.status(200).json({
+            userPosts, savedPosts
+        });
+
+    }catch(error) {
+        console.log(error);
+        res.status(500).send({ message: "Failed to get user." });
+    }
+}
+
+export { getUsers, getUser, updateUser, deleteUser, savePost, profilePosts };
